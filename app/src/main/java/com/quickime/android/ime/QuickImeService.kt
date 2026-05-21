c06be3c80a144ed53c24431a5eeb9a202ff2343a
qiaozhou <qiaozhou@aliyun.com>
2026-05-22 03:06:33 +0800

    fix: 添加缺失的 cancel 导入

--- app/src/main/java/com/quickime/android/ime/QuickImeService.kt
+++ app/src/main/java/com/quickime/android/ime/QuickImeService.kt
@@ -13,6 +13,7 @@ import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.cancel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
